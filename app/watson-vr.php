5ea69d31dd772cd56393b35153f36e928501ac33
Shoichiro Sakaigawa <s.sakaigawa@pumpkinheads.jp>
2017-10-19 12:40:52 +0900

    Create watson-vr.php

---
+++ app/watson-vr.php
@@ -0,0 +1,20 @@
+<pre>
+<?php
+    echo date('l F d, Y G:i:s');
+
+    $watson_json_env = getenv('WATSON_VR');
+    $watson_json_decoded = json_decode($watson_json_env,true);
+    $url=$watson_json_decoded["url"]."/v3/classify?api_key=".$watson_json_decoded["api_key"]."&url=https://raw.githubusercontent.com/phssakaigawa/tsukiji-container/master/app/sushi-1958247_640.jpg&version=2017-10-19";
+    $ch = curl_init($url);
+    echo("---------");
+    
+    curl_setopt($ch,CURLOPT_HEADER,0);
+    
+    $response=curl_exec($ch);
+    $info=curl_getinfo($ch);
+    curl_close($ch);
+    
+    var_dump($response);
+
+?>
+</pre>
