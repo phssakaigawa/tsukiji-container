9a91deb16129e7fb649d3c817f5399e4494c39e5
Shoichiro Sakaigawa <s.sakaigawa@pumpkinheads.jp>
2017-10-19 12:41:42 +0900

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,6 +1,7 @@
-BMXUG つきじ#1
-Kubernetes on Bluemix + DevOpsで CIやってみた
-
+<h1>BMXUG つきじ#1</h1><br>
+<h2>Kubernetes on Bluemix + DevOpsで CIやってみた</h2>
+<br>
+<hr>
 へいおまちっ！！！<br>
 <img src="sushi-1958247_640.jpg">
 
